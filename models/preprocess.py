b64679fc3b9a22be29597c259effe7de4cdab1ed
renuka <renu.stat@gmail.com>
2025-11-01 06:40:13 +0530

    Added .gitignore and dvc.yml

---
+++ models/preprocess.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("data/diabetes.csv")
+
+data.to_csv("data/diabetes.csv")
--- models/train.py
+++ models/train.py
@@ -1,22 +1,29 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
+import mlflow
+import mlflow.sklearn
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-
-df = pd.read_csv('../data/diabetes.csv')
-
-
-X = df.drop('Outcome', axis = 1)
-y = df['Outcome']
-
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = .30, random_state=42)
-X_train.shape, X_test.shape, y_train.shape, y_test.shape
-
-model1 = LogisticRegression()
-logistic_model = model1.fit(X_train, y_train)
-y_test_predict = logistic_model.predict(X_test)
-
+from sklearn.metrics import accuracy_score
+import pandas as pd
 import pickle
-file_name = 'log_model.sav'
-pickle.dump(logistic_model, open(file_name,"wb") )
+
+mlflow.set_tracking_uri("file:./models/mlruns")
+mlflow.set_experiment("diabetes_experiment")
+
+# Load data
+data = pd.read_csv("data/diabetes.csv")
+X = data.drop('Outcome', axis=1)
+y = data['Outcome']
+
+# Train model
+model = LogisticRegression(max_iter=200)
+model.fit(X, y)
+
+# Evaluate
+acc = accuracy_score(y, model.predict(X))
+
+# Log to MLflow
+with mlflow.start_run():
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
+
+# Save locally for FastAPI
+pickle.dump(model, open("models/log_model.sav", "wb"))
