7c2c39a2b75b4ffd496ce08101d1a5da3043d414
renuka <renu.stat@gmail.com>
2025-11-01 07:21:30 +0530

    changes made

--- models/preprocess.py
+++ models/preprocess.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("data/diabetes.csv")
+data = pd.read_csv("~/mlops_docker_demo/data/diabetes.csv")
 
-data.to_csv("data/diabetes.csv")
+data.to_csv("~/mlops_docker_demo/data/diabetes_preprocessed.csv")
